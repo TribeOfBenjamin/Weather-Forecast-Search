1a1deb327e4d20e11279c2f4a89abfb63ca004eb
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-05-08 14:04:53 -0500

    added humidity retrieval and imperial units to API call; changed temperature to account for imperial shift

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
 
 $(document).ready(function(){
 
+    // City search button function
+
+
+
+//  $("#searchButton").on( "click", function() {
+      
+//    let cityNameInput = $("#cityNameInput");
+
+//    localStorage.setItem("City Name", cityNameInput.val());
+
+
+
+
     let APIKey = "a1e0d56642d0b33ce92ae916a2c804ff";
-    //Hard coded for London at the moment
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=" + APIKey;
+    // Hard coded for London at the moment
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&units=imperial&appid=" + APIKey;
+    //let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityNameInput.val() + "&appid=" + APIKey;
+
 
     $.ajax({
       url: queryURL,
@@ -11,21 +26,27 @@ $(document).ready(function(){
     }).then(function(response) {
 
       console.log(response);
-      let temperature = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+      // Gets and displays temperature
+      let temperature = response.list[0].main.temp;
       let temperatureInteger = Math.floor(temperature);
+      $("#currentTemp").append(" " + temperatureInteger + String.fromCharCode(176));
       console.log(temperatureInteger);
+
+      // Gets and displays humidity
+      let humidity = response.list[0].main.humidity;
+      $("#currentHumid").append(" " + humidity + String.fromCharCode(37));
+      console.log(humidity);
+
+      // Gets and displays wind speed
 
     });
 
+  //});
+
+
+    
     // Generates Date and Time in Header
     let todaysDate = $("<p>").text(moment().format("dddd, MMMM Do, YYYY"));
     $("#todaysDate").append(todaysDate);
-
-    console.log(todaysDate);
-
-
-
-
-
     
 })
